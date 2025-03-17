24100486546e5d10fa5d2e8ebf2df02b2fe7408f
la-inge-shop <mildredgomez989@gmail.com>
2025-03-17 09:20:19 -0600

    17/03/2025 09:21

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
-/*!
-* Start Bootstrap - Business Casual v7.0.9 (https://startbootstrap.com/theme/business-casual)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
-*/
-// Highlights current date on contact page
-window.addEventListener('DOMContentLoaded', event => {
-    const listHoursArray = document.body.querySelectorAll('.list-hours li');
-    listHoursArray[new Date().getDay()].classList.add(('today'));
-})
+// Controlar el scroll automático entre secciones
+let currentSectionIndex = 0;
+const sections = document.querySelectorAll('.section');
+
+function scrollToSection(index) {
+  if (index >= 0 && index < sections.length) {
+    currentSectionIndex = index;
+    sections[currentSectionIndex].scrollIntoView({
